317275e862958b9e99d0d6d90736b6de999fcf5a
Leo Developer <leodeveloper@protonmail.com>
2022-11-25 12:06:27 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+
+# Cloudflare Turnstile Types
+
+Unofficial types for [Cloudflare Turnstile](https://challenges.cloudflare.com/).
+
+## Install
+
+```sh
+npm install -D turnstile-types
+```
+
+## Usage
+
+Add `turnstile-types` to the types array in your `tsconfig.json`:
+
+```json
+{
+  "compilerOptions": {
+    "target": "ES2020",
+    "module": "CommonJS",
+    "lib": ["ES2020"],
+    "types": ["turnstile-types"]
+  }
+}
+```
+
---
+++ index.d.ts
@@ -0,0 +1,33 @@
+
+declare global {
+    interface Window {
+        turnstile: {
+            render: (
+                idOrContainer: string | HTMLElement,
+                options: TurnstileOptions
+            ) => string;
+            reset: (widgetIdOrContainer?: string | HTMLElement) => void;
+            getResponse: (
+                widgetIdOrContainer?: string | HTMLElement
+            ) => string | undefined;
+            remove: (widgetIdOrContainer?: string | HTMLElement) => void;
+        };
+    }
+}
+
+export interface TurnstileOptions {
+    sitekey: string;
+    action?: string;
+    cData?: string;
+    callback?: (token: string) => void;
+    "error-callback"?: () => void;
+    "expired-callback"?: () => void;
+    "timeout-callback"?: () => void;
+    theme?: "light" | "dark" | "auto"; // defaults to auto
+    tabindex?: number;
+    "response-field"?: boolean; // defaults to true
+    "response-field-name"?: string; // defaults to cf-turnstile-response
+    size?: "normal" | "invisible" | "compact";
+    retry?: "auto" | "never";
+    "retry-interval"?: number; // up to 15m (900_000) in ms, default is 8s
+}
